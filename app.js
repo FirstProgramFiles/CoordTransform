3f9c2990ece301dfc77f362b13ed62e9dcc8f1d7
Aleksandr <aterebeev@mail.ru>
2022-12-09 10:07:01 +0500

    modified:   app.js

--- app.js
+++ app.js
@@ -14,15 +14,27 @@ app.use(multer({ storage: storageConfig }).single("filedata"));
 
 app.post("/Transform", function (req, res) {
 	data = req.body.Coords;
+	CoordinateSystem = (req.body.CoordinateSystem).replace(/\r\n/g, '');
 	Coord = JSON.parse(data);
 	console.log(Coord);
+	console.log(CoordinateSystem);
 	const longitude = Coord.longitude
 	const latitude = Coord.latitude;
 	const LonTransform = [];
 	const LatTransform = [];
 
-	//МСК-65
-	const fromProjection = '+proj=tmerc +lat_0=0 +lon_0=142.71666666667 +k=1 +x_0=1300000 +y_0=-4516586.439 +ellps=krass +towgs84=23.57,-140.95,-79.8,0,0.35,0.79,-0.22 +units=m +no_defs';
+	switch (CoordinateSystem) {
+		case 'МСК-65':
+			var fromProjection = '+proj=tmerc +lat_0=0 +lon_0=142.71666666667 +k=1 +x_0=1300000 +y_0=-4516586.439 +ellps=krass +towgs84=23.57,-140.95,-79.8,0,0.35,0.79,-0.22 +units=m +no_defs';
+			break;
+		case 'ГСК-2011':
+			var fromProjection = '+proj=tmerc +lat_0=0 +lon_0=39 +k=1 +x_0=7500000 +y_0=0 +a=6378136.5 +rf=298.2564151 +towgs84=0.013,-0.092,-0.03,-0.001738,0.003559,-0.004263,0.0074 +units=m +no_defs';
+			break;
+		case 'СК-42':
+			var fromProjection = '+proj=longlat +datum=WGS84 +to +proj=tmerc +ellps=krass +towgs84=23.57,-140.95,-79.8,0,0.35,0.79,-0.22 +lon_0=39 +x_0=7500000';
+			break;
+	}
+
 	const toProjection = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'; //WGS84
 
 	for (let i = 0; i < longitude.length; i++) {
